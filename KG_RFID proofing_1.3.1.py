e820f3a9031f27eb6fef4ca05b14088325856a9c
kgu <35379529+karengu30@users.noreply.github.com>
2021-01-10 16:58:48 -0500

    Update KG_RFID proofing_1.3.1.py

--- KG_RFID proofing_1.3.1.py
+++ KG_RFID proofing_1.3.1.py
@@ -1,6 +1,12 @@
-#after two months, this code works!
-#at some point, it appears to multiply the occurence of every datetime RFID reading by 4
-    #this is easily correctable by dividing value counts by 4 before appending them to freqlist, but why is it happening?
+### to run this code ###
+# >>> e = ExcelAnalysis("/Users/karen/OneDrive/Documents/0_RESEARCH/RFID/11-06-2020_tagmanager.xlsx")
+# >>> e.something('3D6.1D59B07978','11/04/2020','11/05/2020')
+# >>> e.exportdata()
+# then you can continue changing parameters in e.something without reloading the entire script 
+# for example type in next:
+# >>> e.something('3D6.1D59B0796C','11/04/2020','11/05/2020')
+# >>> e.exportdata()
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -90,61 +96,3 @@ class ExcelAnalysis:
         plt.xlabel('Date and Time')
         plt.ylabel('Number of RFID Readings')
         plt.show()
-
-
-"""
-input_file = '/Users/johnmerlo-coyne/Desktop/Juntti Lab Stuff/RFID Project/RFID Output Sheets/10.23.20_karen.xlsx' #paste filepath here, including file name- end with .xlsx
-target_fishid = '3D6.1D59B07986' #from spreadsheet, paste fishid name here in quotes
-
-start_date, end_date = str('09/25/2020'), str('09/26/2020')
-def convertdate(input_date): #function converts a string of text into a readable date format (requires m/d/y input)
-    output_date = datetime.strptime(input_date, '%m/%d/%Y') #converts input date (string) into a datetime function
-    return(output_date)
-
-start_date = convertdate(start_date) #converts start date to date format
-end_date = convertdate(end_date)
-
-df = pd.read_excel(input_file) #import RFID output sheet from filepath
-df.columns = df.columns.str.replace(" ","") #remove spaces from column names
-print(df.columns) #prints a list of column names
-
-df_culled = df[(df['HEXTagID'] == target_fishid) & (df['ScanDate'].apply(convertdate) >= start_date) & (df['ScanDate'].apply(convertdate) <= end_date)]
-#^culls RFID sheet to only readings for selected fishid and range of dates
-
-df_culled['DateTimeRaw'] = df_culled.ScanDate + " " + df_culled.ScanTime #combines ScanDate and ScanTime string into one column
-df_out = pd.DataFrame(pd.to_datetime(df_culled['DateTimeRaw'], format = '%m/%d/%Y %H:%M:%S.%f')) #converts datetime string column to datetime format
-df_out['DateTimeShort'] = df_out['DateTimeRaw'].values.astype('datetime64[h]') #cuts the datetime column to just month, day and hour
-
-possible = pd.date_range(start_date, end_date, freq='h') #creates list of all datetimes between start dayhour and end dayhour
-possible_frame = possible.to_frame() #turns it into a dataframe
-
-datetimelist = [] #creates empty list to hold all dayhours between target times
-freqlist = [] #creates empty list to hold frequencies of RFID readings for each dayhour (0 for some)
-for i in possible: #for each dayhour in the range...
-    datetimelist.append(i) #add it to datetimelist
-    if (i in df_out['DateTimeShort'].values): #if that dayhour is in the list of dayhours with an RFID reading...
-        freqlist.append(df_out['DateTimeShort'].value_counts()[i]) #return the number of RFID readings for that hour
-    else: #if there are no RFID readings for that hour (dayhour not in RFID sheet)
-        freqlist.append(0) #add 0 to the list of RFID readings for that dayhour
-d = {'datetimes': datetimelist, 'occurence': freqlist}
-df_datetimefreq = pd.DataFrame(d) #dataframe pairing list of dayhours with number of RFID readings for each dayhour
-print(df_datetimefreq)
-
-print('TESTING SPREADSHEETS AGAINST EACH OTHER!')
-print(df.head(20))
-print(df_out.head(20))
-
-
-fig, ax = plt.subplots(figsize = (8,6)) #creates bar graph
-idx = np.asarray([i for i in range(len(datetimelist))])
-ax.bar(idx, freqlist)
-ax.set_xticks(idx)
-ax.set_xticklabels(datetimelist, rotation=90)
-fig.tight_layout()
-title_name = ('RFID frequency for fish ' + target_fishid + ": " + str(start_date) + " to " + str(end_date))
-#^puts together graph title, using name of fish in question, and target dates
-plt.title(title_name)
-plt.xlabel('Date and Time')
-plt.ylabel('Number of RFID Readings')
-plt.show()
-"""
